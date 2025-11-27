6dadcea7d1b301566168e5e0d48db526abb46ed7
Alvin <alvin.webdev.consular@gmail.com>
2025-11-27 23:57:18 +0800

    WIP. Adding controllers for projects and tasks

--- server/server.js
+++ server/server.js
@@ -4,16 +4,23 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
+import { protect } from "./middlewares/authMiddleware.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
 app.use(express.json());
-app.user = cors();
+app.use(cors());
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("Server is live!"));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
 
 const PORT = process.env.PORT || 5000;
 
